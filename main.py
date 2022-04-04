da3240ec6c16899c0a1f857dfa5cb4b9ccd57bdd
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-04 13:48:10 -0400

    adding SpeechRecognition and Google recognizer

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # AI Test
 # Created by Gabriel Tavares
+
+import speech_recognition as sr
+
+# create a listener for SpeechRecognition
+# Recognizer is used to recognize my voice
+listener = sr.Recognizer()
+
+# Try block to check for errors
+try:
+    # use microphone as source of audio
+    with sr.Microphone as source:
+        print('Listening...')
+        # declare Voice variable to listen to our source
+        voice = listener.listen(source)
+        # once we have the source, use Google to convert
+        # the voice variable into text
+        command = listener.recognize_google(voice)
+        print(command)
+
+except:
+    pass
