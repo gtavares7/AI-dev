d2ed0c2c01fa48db62a5f4177fc2364108663e1e
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-09 15:54:16 -0400

    Added package imports

--- voice-assistant-v2.py
+++ voice-assistant-v2.py
@@ -1,5 +1,14 @@
 # Voice Assistant v2.0
 # Created by Gabriel Tavares
 
-
+import speech_recognition as sr
+import playsound
+from gtts import gTTS
+import random
+from time import ctime
+import webbrowser
+import ssl
+import certifi
+import time
+import os
 
