6a37a6d02d6d7b8cf7722e02af04cba26f1093a2
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-10 09:48:02 -0400

    minor bug fixes

--- voice-assistant-v2.py
+++ voice-assistant-v2.py
@@ -16,15 +16,15 @@ import os
 # initialize a recognizer
 r = sr.Recognizer()
 # listen for audio and convert it to text
-def record_audio (ask=False)
-    with sr.Microphone() as source # microphone as source
+def record_audio (ask=False):
+    with sr.Microphone() as source: # microphone as source
         if ask:
             speak(ask)
         audio = r.listen(source) # listen for audio via source
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio) # convert audio to text
-        except sr.UnknownValueError # error: recognizer does not understand
+        except sr.UnknownValueError: # error: recognizer does not understand
             speak('Sorry, I did not get that')
         except sr.RequestError: # error: Recognizer is not connected
             speak('Sorry, my service is down')
