f7c5a114d0e1148fc2d0d50ee669f521b8fdbcc4
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-10 09:45:42 -0400

    audio recognizer and convert it to text

--- voice-assistant-v2.py
+++ voice-assistant-v2.py
@@ -12,3 +12,21 @@ import certifi
 import time
 import os
 
+
+# initialize a recognizer
+r = sr.Recognizer()
+# listen for audio and convert it to text
+def record_audio (ask=False)
+    with sr.Microphone() as source # microphone as source
+        if ask:
+            speak(ask)
+        audio = r.listen(source) # listen for audio via source
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio) # convert audio to text
+        except sr.UnknownValueError # error: recognizer does not understand
+            speak('Sorry, I did not get that')
+        except sr.RequestError: # error: Recognizer is not connected
+            speak('Sorry, my service is down')
+        print("f >> {voice_data.lower()}") # print what user said
+        return voice_data.lower()
