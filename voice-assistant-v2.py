59076c3ac77c984b7ed6493e3bbd39eae6deb486
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-10 10:05:14 -0400

    added function to respond to the user

--- voice-assistant-v2.py
+++ voice-assistant-v2.py
@@ -12,21 +12,33 @@ import certifi
 import time
 import os
 
-
 # initialize a recognizer
 r = sr.Recognizer()
+
+
 # listen for audio and convert it to text
-def record_audio (ask=False):
-    with sr.Microphone() as source: # microphone as source
+def record_audio(ask=False):
+    with sr.Microphone() as source:  # microphone as source
         if ask:
             speak(ask)
-        audio = r.listen(source) # listen for audio via source
+        audio = r.listen(source)  # listen for audio via source
         voice_data = ''
         try:
-            voice_data = r.recognize_google(audio) # convert audio to text
-        except sr.UnknownValueError: # error: recognizer does not understand
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError:  # error: recognizer does not understand
             speak('Sorry, I did not get that')
-        except sr.RequestError: # error: Recognizer is not connected
+        except sr.RequestError:  # error: Recognizer is not connected
             speak('Sorry, my service is down')
-        print("f >> {voice_data.lower()}") # print what user said
+        print("f >> {voice_data.lower()}")  # print what user said
         return voice_data.lower()
+
+
+# reply to the question
+def speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')  # text to speach(voice)
+    r = random.randint(1, 20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file)  # save audio as .mp3
+    playsound.playsound(audio_file)  # play the audio file
+    print(f"May Day: {audio_file}")  # print what the app said
+    os.remove(audio_file)  # remove audio file
